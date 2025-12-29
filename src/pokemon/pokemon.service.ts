48fd8d1fc568126b8e99f728e4167c4dfefbcbe5
LuisRivera2016 <luisi.najera@gmail.com>
2025-12-29 17:17:15 -0600

    Pokedex CRUD

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { isValidObjectId, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -24,8 +25,15 @@ export class PokemonService {
 
   }
 
-  findAll() {
-    return this.pokemonModel.find();
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.pokemonModel.find()
+    .limit(limit)
+    .skip(offset)
+    .sort({
+      no: 1
+    })
+    .select('-__v');
   }
 
   async findOne(term: string) {
@@ -83,9 +91,9 @@ export class PokemonService {
     // const pokemon = await this.findOne(term);
     // await pokemon.deleteOne();
     // const result = await this.pokemonModel.findByIdAndDelete(term);
-    const { deletedCount } = await this.pokemonModel.deleteOne({_id: term});
-    if ( deletedCount === 0)
-        throw new BadRequestException(`Pokemon with id "${term}" not found`)
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: term });
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${term}" not found`)
     return;
   }
 
